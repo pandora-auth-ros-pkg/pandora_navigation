c8cf22284b9e1bd49ccc22e05c7b3b66426fdef0
Dimitrios Kirtsios <dimkirt@hotmail.com>
2015-05-30 21:02:28 +0300

    PATCH add comments to mock_sonar_sensor

--- range_sensor_layer/scripts/mock_sonar_sensor.py
+++ range_sensor_layer/scripts/mock_sonar_sensor.py
@@ -8,12 +8,16 @@ def talker():
   rate = rospy.Rate(10)  # 10 Hz
 
   # Msg creation
+
   r = Range()
-  r.header.frame_id = '/right_sonar_link'
+  r.header.frame_id = '/base_link'
   r.field_of_view = 25*3.14/180
   r.max_range = 100
   r.range = 1
-
+  # real sonar
+  # fov: 1.04 olo
+  # max range 4m
+  #min range 0.2m
   while not rospy.is_shutdown():
     r.header.stamp = rospy.Time.now()
     rospy.loginfo(r)
