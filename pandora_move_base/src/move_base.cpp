72312d5d6d11e43958794e8cce0f1e22ae63bd4d
geromidg <geromidg@gmail.com>
2015-07-19 00:33:04 +0800

    Change params for stdr testing

--- pandora_move_base/src/move_base.cpp
+++ pandora_move_base/src/move_base.cpp
@@ -102,7 +102,7 @@ namespace move_base {
     private_nh.param("local_costmap/circumscribed_radius", circumscribed_radius_, 0.46);
     private_nh.param("clearing_radius", clearing_radius_, circumscribed_radius_);
     private_nh.param("conservative_reset_dist", conservative_reset_dist_, 3.0);
-    private_nh.param("backwards_vel", backwards_vel_, -0.05);
+    private_nh.param("backwards_vel", backwards_vel_, -0.5);
 
     private_nh.param("shutdown_costmaps", shutdown_costmaps_, false);
     private_nh.param("clearing_rotation_allowed", clearing_rotation_allowed_, true);
@@ -494,12 +494,12 @@ namespace move_base {
 
       //we'll try to clear out space with any user-provided recovery behaviors
       case CLEARING:
-        ROS_INFO("move_base In clearing/recovery state");
+        ROS_WARN("In clearing/recovery state");
 
-        recovery_behavior_enabled_ = true;  // Fix me (tsompania)
         //we'll invoke whatever recovery behavior we're currently on if they're enabled
+        recovery_behavior_enabled_ = true;  // FIXME
         if(recovery_behavior_enabled_ && recovery_index_ < recovery_behaviors_.size()){
-          ROS_WARN("move_base_recovery, Executing behavior %u of %zu", recovery_index_, recovery_behaviors_.size());
+          ROS_WARN("Executing behavior %u of %zu", recovery_index_, recovery_behaviors_.size());
           recovery_behaviors_[recovery_index_]->runBehavior();
 
           //we at least want to give the robot some time to stop oscillating after executing the behavior
